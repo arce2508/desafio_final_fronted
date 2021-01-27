5aef37986a5f9cd5d8e773a952a10f442b57ae57
arce <arcegutierrez.2508@gmail.com>
2021-01-27 12:20:17 -0600

    archivos js adjuntos

--- js/Get.js
+++ js/Get.js
@@ -1,22 +1,21 @@
-const getPost() =>{
+const getPost = () => {
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
-    if ( this.readyState === 4 && this.status === 200){
-      let response = JSON.parse (this.responseText);
-      console.log ( response)
+    if (this.readyState === 4 && this.status === 200) {
+      let response = JSON.parse(this.responseText);
+      console.log(response);
     }
-  }
-  xhttp.open (
-    "GET"
+  };
+  xhttp.open(
+    "GET",
     "https://finalproject-f130e-default-rtdb.firebaseio.com/Post/.json",
-   true 
-  )
-  xhttp.send ()
+    true
+  );
+  xhttp.send();
 };
 
-const printPostData () => {
-  const wrapper = document.creaElement ('article');
-  let articleObject = { article}
-  console.log (articleObject )
-}
-
+const printPostData = () => {
+  const wrapper = document.creaElement("article");
+  let articleObject = { article };
+  console.log(articleObject);
+};
